d6973b91f6a87a69e2f28e4edf5b0dd67b4825ab
Teacher <teacher@techer.com>
2024-12-03 19:15:55 +0300

    Добавлено решение

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print('My first git repo')
-print('Второй коммит')
+# lst = [x for x in input().split('; ')
+#        if len(x) % 2 != 0 and x[len(x) // 2] in 'aeyuio']
+# print(input().join(lst))
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+
+test = c[-1]
+res = list((set(a) ^ set(b) ^ set(c)) - (set(a) & set(b) & set(c)))
+print('!!'.join([str(x) for x in res if x < test]))
